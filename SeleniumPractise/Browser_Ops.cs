ff8a5bd40a3b16d13a82d2874462ab1d081e5e9a
jsahota91 <jsahota@spartaglobal.com>
2022-07-16 23:05:14 +0100

    first commit

---
+++ SeleniumPractise/Browser_Ops.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumPractise
+{
+    public class Browser_Ops
+    {
+        IWebDriver driver;
+
+        public void InitBrowser()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        public string Title()
+        {
+            return driver.Title;
+        }
+
+        public void GoTo(string url)
+        {
+            driver.Url = url;
+        }
+
+        public void Close()
+        {
+            driver.Quit();
+        }
+
+        public IWebDriver GetDriver()
+        {
+            return driver;
+        }
+    }
+
+}
---
+++ SeleniumPractise/UnitTest1.cs
@@ -0,0 +1,51 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumPractise;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace PureGym
+{
+    public class SeleniumPractise
+    {
+
+        Browser_Ops browser = new Browser_Ops();
+        string url = "https://demowf.aspnetawesome.com/";
+        public IWebDriver driver;
+
+        [SetUp]
+        public void InitialiseBrowser()
+        {
+            browser.InitBrowser();          
+        }
+
+        [Test]
+        public void TestBrowser()
+        {
+            browser.GoTo(url);
+            Thread.Sleep(400);
+            driver = browser.GetDriver();
+            IWebElement dropdown = driver.FindElement(By.Id("ContentPlaceHolder1_Meal"));
+            dropdown.SendKeys("Tomato");
+            dropdown.SendKeys(Keys.Return);
+            Thread.Sleep(400);
+            IWebElement element2 = driver.FindElement(By.XPath("//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Celery']"));
+            element2.Click();
+            IWebElement comboControl = driver.FindElement(By.CssSelector("#ContentPlaceHolder1_AllMealsCombo-awed"));
+            comboControl.Clear();
+            comboControl.SendKeys("Almond");
+            driver.FindElement(By.CssSelector("li[data-val$= 'Almond']")).Click();
+            //comboControl.SendKeys(Keys.Return);
+            Console.WriteLine("test1");
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            browser.Close();
+        }
+        
+    }
+}
